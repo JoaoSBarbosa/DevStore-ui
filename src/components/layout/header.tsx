510c20638f36475550d00fdeb3a67ac068cb4d95
João Barbosa <contato.jsbarbosa@gmail.com>
2026-03-27 21:10:53 -0300

    :sparkles: Header - implementação inicial do logo e botões

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,34 +1,27 @@
+"use client";
+
+import Image from "next/image";
+
 export default function Header() {
   return (
-    <header className="border border-cyan-400">
-
+    <header className="bg-white border-b border-gray-200">
       <div className="bg-black text-white p-4 text-center">
-
-        <strong>FRETE GRATIS</strong>
-        para todo o Nordeste nas compras acima de R$ 199,00. <strong> APROVEITA!</strong>
+        <strong>FRETE GRATIS</strong> para todo o Nordeste nas compras acima de
+        R$ 199,00. <strong> APROVEITA!</strong>
       </div>
 
-      <nav>
-        <ul>
-          <li>
-            <a href="">Home</a>
-          </li>
-          <li>
-            <a href="">Produtos</a>
-          </li>
-          <li>
-            <a href=""></a>
-          </li>
-          <li>
-            <a href=""></a>
-          </li>
-          <li>
-            <a href=""></a>
-          </li>
-        </ul>
-      </nav>
+      <div className="p-6 w-full max-w-6xl mx-auto flex items-center justify-between">
+        <div className="">
+          <Image
+            src="/assets/ui/logo-black.png"
+            alt="DevSTore-logo"
+            width={120}
+            height={40}
+          />
+        </div>
 
-
+        <div className="">Botoes</div>
+      </div>
     </header>
   );
 }
