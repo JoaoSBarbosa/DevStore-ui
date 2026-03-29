cabab5f1e8b0de27721bcd9501610aaec4f1a9d3
João Barbosa <contato.jsbarbosa@gmail.com>
2026-03-29 16:39:31 -0300

    correção de conflitos

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,12 @@
-
 import { Metadata } from "next";
 import "./globals.css";
+import { Poppins } from "next/font/google";
 
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["400", "500", "700"],
+  display: "swap",
+});
 
 export const metadata: Metadata = {
   title: "DevStore",
@@ -10,16 +15,12 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
-    <html lang="pt-br">
-      <body
-        className={`antialiased`}
-      >
-        {children}
-      </body>
+    <html lang="pt-BR">
+      <body className={`${poppins.className} antialiased`}>{children}</body>
     </html>
   );
 }
