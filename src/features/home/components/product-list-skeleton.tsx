a04d337831ff3aa5af7127c3d569abe114dd27b0
João Barbosa <contato.jsbarbosa@gmail.com>
2026-03-31 22:08:33 -0300

    :sparkles: Implementado Skeleton para load de carregando

---
+++ src/features/home/components/product-list-skeleton.tsx
@@ -0,0 +1,17 @@
+export const ProductListSkeleton = () => {
+  return (
+    <div className="lg:my-4">
+      <div className="flex gap-4 flex-col">
+        <div className="bg-gray-200 rounded w-52 h-8 mx-auto lg:mx-0 animate-pulse"></div>
+        <div className="bg-gray-200 rounded w-64 h-5 mx-auto lg:mx-0 animate-pulse"></div>
+      </div>
+
+      <div className="grid grid-cols-1 lg:grid-cols-4 gap-8 mt-9">
+        <div className="bg-gray-200 h-80 rounded animate-pulse"></div>
+        <div className="bg-gray-200 h-80 rounded animate-pulse"></div>
+        <div className="bg-gray-200 h-80 rounded animate-pulse"></div>
+        <div className="bg-gray-200 h-80 rounded animate-pulse"></div>
+      </div>
+    </div>
+  );
+};
--- src/features/home/home.tsx
+++ src/features/home/home.tsx
@@ -2,6 +2,7 @@ import { banners } from "./data/banner-data";
 import { Banners } from "./sections/banner/banners";
 import { storeBenefits } from "./data/store-benefits-data";
 import { StoreBenefitsContainer } from "./sections/store-benefits/store-benefits-container";
+import { ProductListSkeleton } from "./components/product-list-skeleton";
 
 export default function Home() {
   const data = banners;
@@ -9,6 +10,7 @@ export default function Home() {
     <main className="max-w-6xl mx-auto p-6">
       <Banners banners={data} />
       <StoreBenefitsContainer benefits={storeBenefits} />
+      <ProductListSkeleton />
     </main>
   );
 }
