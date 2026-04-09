4f7d6cfe370f434029d84625537f7868e0245ea7
João Barbosa <contato.jsbarbosa@gmail.com>
2026-04-08 21:33:05 -0300

    :sparkles: Implementado accordion

--- src/features/product-listing/components/filter-group.tsx
+++ src/features/product-listing/components/filter-group.tsx
@@ -1,5 +1,9 @@
+"use client";
 import { useQueryString } from "@/shared/hooks/use-querystring";
 import { ProductFiltered } from "@/shared/types/mock/product-type";
+import { FilterItem } from "./filter-item";
+import Image from "next/image";
+import { useState } from "react";
 
 type FilterGroupProps = {
   filters?: ProductFiltered;
@@ -7,21 +11,33 @@ type FilterGroupProps = {
 
 export const FilterGroup = ({ filters }: FilterGroupProps) => {
   const queryString = useQueryString();
-
+  const [accordionOpen, setAccordionOpen] = useState<boolean>(true);
   const filterValue = queryString.get("filters") ?? "";
+
+  const toggleAccordion = () => {
+    setAccordionOpen((prev) => !prev);
+  };
   return (
     <div className="w-full mb-8">
       <div className="flex justify-between items-center border-b border-gray-200 pb-4">
         <h2 className="flex-1 font-bold text-xl">{filters?.title}</h2>
-        <div>...</div>
+        <div className="size-8 flex justify-center items-center">
+          <Image
+            src={"/assets/ui/arrow-left-s-line.png"}
+            alt="arrow"
+            width={24}
+            height={24}
+            onClick={toggleAccordion}
+            className={`transition-all cursor-pointer ${!accordionOpen ? "rotate-180" : "rotate-0"}`}
+          />
+        </div>
       </div>
 
-      <div className="text-gray-500 flex text-lg flex-col gap-4 pt-8">
+      <div
+        className={`flex flex-col gap-4 overflow-y-hidden ${accordionOpen ? "h-auto pt-8" : "h-0 pt-0"} transition-all`}
+      >
         {filters?.options.map((option) => (
-          <label key={option.id} className="flex items-center gap-4">
-            <input type="checkbox" className="size-6" />
-            {option.label}
-          </label>
+          <FilterItem key={option.id} filterOption={option} />
         ))}
       </div>
     </div>
